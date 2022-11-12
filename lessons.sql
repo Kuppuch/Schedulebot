c8d9c78e8c4c9dea8c0ffb669ebd64eaee0faa9f
Kuppuch <55031960+Kuppuch@users.noreply.github.com>
2022-11-12 14:02:14 +0300

    Add files via upload

---
+++ lessons.sql
@@ -0,0 +1,28 @@
+INSERT INTO public.lessons (id, created_at, updated_at, deleted_at, name, start, "end", source, type, day) VALUES (2, null, null, null, 'Коммуникативные и образовательные
+проблемы профессиональной
+деятельности', '2022-09-29 15:50:00.194000 +00:00', '2022-09-29 17:20:23.329000 +00:00', 'https://meet.jit.si/SK_ISPI', 'знаменатель', 'понедельник');
+INSERT INTO public.lessons (id, created_at, updated_at, deleted_at, name, start, "end", source, type, day) VALUES (3, null, null, null, 'Коммуникативные и образовательные
+проблемы профессиональной
+деятельности', '2022-09-29 17:40:00.534000 +00:00', '2022-09-29 19:10:23.329000 +00:00', 'https://meet.jit.si/SK_ISPI', 'знаменатель', 'понедельник');
+INSERT INTO public.lessons (id, created_at, updated_at, deleted_at, name, start, "end", source, type, day) VALUES (4, null, null, null, 'Программирование графических
+приложений', '2022-09-29 16:30:00.183000 +00:00', '2022-09-29 17:20:00.122000 +00:00', 'https://meet.jit.si/IJ_Ispi_pgp2', 'числитель', 'вторник');
+INSERT INTO public.lessons (id, created_at, updated_at, deleted_at, name, start, "end", source, type, day) VALUES (5, null, null, null, 'Модели и методы планирования
+экспериментов, обработки экспериментальных
+данных ', '2022-09-29 17:40:00.526000 +00:00', '2022-09-29 19:10:00.521000 +00:00', 'https://meet.jit.si/jaсob', 'числитель', 'вторник');
+INSERT INTO public.lessons (id, created_at, updated_at, deleted_at, name, start, "end", source, type, day) VALUES (6, null, null, null, 'Программирование графических
+приложений', '2022-09-29 16:30:00.183000 +00:00', '2022-09-29 17:20:00.122000 +00:00', 'https://meet.jit.si/IJ_Ispi_pgp2', 'знаменатель', 'вторник');
+INSERT INTO public.lessons (id, created_at, updated_at, deleted_at, name, start, "end", source, type, day) VALUES (7, null, null, null, 'Модели и методы планирования
+экспериментов, обработки экспериментальных
+данных ', '2022-09-29 17:40:00.526000 +00:00', '2022-09-29 19:10:00.521000 +00:00', 'https://meet.jit.si/jaсob', 'знаменатель', 'вторник');
+INSERT INTO public.lessons (id, created_at, updated_at, deleted_at, name, start, "end", source, type, day) VALUES (8, null, null, null, 'Системная инженерия', '2022-09-29 15:50:00.899000 +00:00', '2022-09-29 17:20:00.921000 +00:00', 'https://meet.jit.si/SystemEngineering', 'числитель', 'среда');
+INSERT INTO public.lessons (id, created_at, updated_at, deleted_at, name, start, "end", source, type, day) VALUES (9, null, null, null, 'Системная инженерия', '2022-09-29 17:40:00.558000 +00:00', '2022-09-29 19:10:00.953000 +00:00', 'https://meet.jit.si/SystemEngineering', 'числитель', 'среда');
+INSERT INTO public.lessons (id, created_at, updated_at, deleted_at, name, start, "end", source, type, day) VALUES (10, null, null, null, 'Облачные технологии', '2022-09-29 15:50:00.899000 +00:00', '2022-09-29 17:20:00.921000 +00:00', 'https://discord.gg/BSzF9URu', 'знаменатель', 'среда');
+INSERT INTO public.lessons (id, created_at, updated_at, deleted_at, name, start, "end", source, type, day) VALUES (11, null, null, null, 'Облачные технологии', '2022-09-29 17:40:00.558000 +00:00', '2022-09-29 19:10:00.953000 +00:00', 'https://discord.gg/BSzF9URu', 'знаменатель', 'среда');
+INSERT INTO public.lessons (id, created_at, updated_at, deleted_at, name, start, "end", source, type, day) VALUES (12, null, null, null, 'Интеграция кроссплатформенных
+программных систем', '2022-09-29 16:00:00.558000 +00:00', '2022-09-29 17:20:00.558000 +00:00', 'https://us06web.zoom.us/j/89357990217?pwd=L2tPdkNQSURlU3pWVGVEMzNQeTdZdz09', 'числитель', 'четверг');
+INSERT INTO public.lessons (id, created_at, updated_at, deleted_at, name, start, "end", source, type, day) VALUES (13, null, null, null, 'Интеграция кроссплатформенных
+программных систем', '2022-09-29 16:00:00.558000 +00:00', '2022-09-29 17:20:00.558000 +00:00', 'https://us06web.zoom.us/j/89357990217?pwd=L2tPdkNQSURlU3pWVGVEMzNQeTdZdz09', 'знаменатель', 'четверг');
+INSERT INTO public.lessons (id, created_at, updated_at, deleted_at, name, start, "end", source, type, day) VALUES (14, null, null, null, 'Философские и социальные проблемы
+информационного общества', '2022-09-29 17:40:00.848000 +00:00', '2022-09-29 19:10:00.682000 +00:00', null, 'числитель', 'четверг');
+INSERT INTO public.lessons (id, created_at, updated_at, deleted_at, name, start, "end", source, type, day) VALUES (15, null, null, null, 'Интеграция кроссплатформенных
+программных систем', '2022-09-29 17:40:00.558000 +00:00', '2022-09-29 19:10:00.558000 +00:00', 'https://discord.gg/BSzF9URu', 'знаменатель', 'четверг');
